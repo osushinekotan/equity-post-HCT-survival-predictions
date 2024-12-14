e0dea3dc3f395533ba45ac21bc12ef766480c28d
osushinekotan <spmt3710@gmail.com>
2024-12-14 12:56:32 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # equity-post-HCT-survival-predictions
+
 https://www.kaggle.com/competitions/equity-post-HCT-survival-predictions
+
+```bash
+export KAGGLE_USERNAME=
+export KAGGLE_KEY=
+```
+
+```bash
+uv run pre-commit run -a
+```
---
+++ cibmtr/load_dataset.py
@@ -0,0 +1,23 @@
+import fire
+import kaggle
+import rootutils
+
+rootutils.setup_root(search_from=".", dotenv=True, pythonpath=True)
+
+from cibmtr.kaggle_utils.dataset import download_kaggle_competition_dataset
+
+kaggle_client = kaggle.KaggleApi()
+kaggle_client.authenticate()
+
+
+def main(force: bool = False, out_dir: str = "data/inputs") -> None:
+    download_kaggle_competition_dataset(
+        client=kaggle_client,
+        competition="equity-post-HCT-survival-predictions",
+        out_dir=out_dir,
+        force=force,
+    )
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
